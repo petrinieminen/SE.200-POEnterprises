ca07fbecbf15b48e89bc0f292a61b2d075da2cc6
pn431258 <petri.o.nieminen@tuni.fi>
2024-11-21 18:27:35 +0200

    added coveralls

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,14 @@
 module.exports = {
     transform: {
-      '^.+\\.js$': 'babel-jest', // Use Babel for JavaScript files
+      '^.+\\.js$': 'babel-jest', // Use Babel to transform JavaScript files
     },
+    collectCoverage: true, // Enable coverage collection
     collectCoverageFrom: [
-      'src/**/*.js',
+      'src/**/*.js', // Include all .js files in src/
       '!src/.internal/**', // Exclude the .internal directory
+      '!src/**/index.js', // Optionally exclude index files
     ],
+    coverageDirectory: 'coverage', // Output coverage reports to the "coverage" folder
+    coverageReporters: ['text', 'lcov'], // Generate both text and lcov reports
+    testEnvironment: 'node', // Use Node.js environment for tests
   };
